44af91db6c15030d1edf17b9f64ee362ba50a75f
dazocc <davison.oliveira@dsfnet.com.br>
2020-09-19 15:40:53 -0400

    #ajuste eureka

---
+++ src/main/java/io/github/sbcloudrace/sbsession/config/Config.java
@@ -0,0 +1,9 @@
+package io.github.sbcloudrace.sbsession.config;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+@EnableEurekaClient
+@Configuration
+public class Config {
+}
